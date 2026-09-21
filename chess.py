016172627a8415290f17254f74524820378a0953
unknown <i.am.matvii.tertyshnyi@gmail.com>
2026-09-21 04:01:29 +0200

    Added possible moves dictionary(only pawns now). Possible moves are now displayed after choosing pawns.

--- chess.py
+++ chess.py
@@ -25,6 +25,12 @@ class MainWindow(QtWidgets.QMainWindow):
             ["P", "P", "P", "P", "P", "P", "P", "P"],
             ["R", "N", "B", "Q", "K", "B", "N", "R"]
         ]
+        self.selected_square = None
+        self.piece_possible_moves = {
+            "p": [(0, 1), (0,2), (1,1), (-1, 1)],
+            "P":[(0, -1), (0,-2), (-1,-1), (1, -1)]
+            }
+        
     def draw_chessboard(self):
         canvas = self.label.pixmap()
         painter = QtGui.QPainter(canvas)
@@ -46,11 +52,12 @@ class MainWindow(QtWidgets.QMainWindow):
                 painter.drawRect(x, y, square_size, square_size)
         painter.end()
         self.label.setPixmap(canvas)
-        
+    #function that reads the chessboard matrix
     def get_all_pieces(self):
         for i in range(len(self.board_state)):
             for j in range(len(self.board_state[i])):
                 yield i, j, self.board_state[i][j]
+    
 
     def draw_chess_pieces(self):
         canvas = self.label.pixmap()
@@ -59,7 +66,8 @@ class MainWindow(QtWidgets.QMainWindow):
         font = QFont("Arial", 20, QFont.Weight.Bold)
         painter.setFont(font)
         for i, j, piece in self.get_all_pieces():
-            painter.drawText((j*64)+27, (i*64)+27, f"{piece}")
+            if piece != ".":
+                painter.drawText((j*64)+27, (i*64)+27, f"{piece}")
         painter.end()
         self.label.setPixmap(canvas)
         
@@ -67,19 +75,32 @@ class MainWindow(QtWidgets.QMainWindow):
         x = int(event.position().x())
         y = int(event.position().y())
         
-        col = (x // 64)+1
-        row = (y // 64)+1
+        col = (x // 64)
+        row = (y // 64)
         if event.button() == Qt.MouseButton.LeftButton:
-            for i, j, piece in self.get_all_pieces():
-                if(self.board_state[col-1][row-1] == piece): 
-                    print(f"u just left clicked {col, row, piece}")
+            print(f"u just clicked {col+1, row+1, self.board_state[row][col]}")
+            self.selected_square = (row, col)
+            print(self.selected_square)
+            self.calculate_possible_moves(row, col, self.board_state[row][col])
             canvas = self.label.pixmap()
             painter = QtGui.QPainter(canvas)
             painter.setBrush(QtGui.QBrush(Qt.GlobalColor.black))
-            painter.drawEllipse((((col-1)*64)+27), (((row-1)*64)+27), 10,10)
+            painter.drawEllipse((((col)*64)+27), (((row)*64)+27), 10,10)
             painter.end()
             self.label.setPixmap(canvas)
-
+            
+    def calculate_possible_moves(self, row, col, piece):
+        for possible_x, possible_y in self.piece_possible_moves[piece]:
+            final_x = col + possible_x
+            final_y = row + possible_y
+            print(f"Moves available for the {piece} are: {final_x}, {final_y}")
+            canvas = self.label.pixmap()
+            painter = QtGui.QPainter(canvas)
+            painter.setBrush(QtGui.QBrush(Qt.GlobalColor.black))
+            painter.drawEllipse((((final_x)*64)+27), (((final_y)*64)+27), 10,10)
+            painter.end()
+            self.label.setPixmap(canvas)
+        
 
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
